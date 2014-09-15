71d55e6334837c4d964c45c57b5f6ebae28f6522
Håvar Aambø Fosstveit <havfo@dessie.fosstveit.no>
2014-09-16 00:13:02 +0200

    Lots of changes to fix overall performance

---
+++ src/net/fosstveit/atbuss/UpdateActivity.java
@@ -0,0 +1,13 @@
+package net.fosstveit.atbuss;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UpdateActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_update);
+	}
+}
